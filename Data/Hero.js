69a78ac5673729f97c169916e6fb6493963b0c29
muhammadsaad91 <saadattari91@gmail.com>
2023-06-07 21:55:36 +0500

    first commit

---
+++ Data/Hero.js
@@ -0,0 +1,8 @@
+const Hero = {
+      Heading : "The best offer for your business",
+      Button : "Get Started",
+        Image : "https://tecdn.b-cdn.net/img/new/ecommerce/vertical/004.jpg"
+
+ }
+
+export default Hero;
--- components/Hero.js
+++ components/Hero.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Data from '../Data/Hero.js'
 
 export default function Hero() {
   return (
@@ -32,16 +33,16 @@ export default function Hero() {
             <div
               className="block rounded-lg  px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-[hsla(0,0%,5%,0.55)] dark:shadow-black/20 md:px-12 lg:-mr-14 backdrop-blur-[30px]">
               <h1 className="mt-2 mb-16 text-4xl font-bold tracking-tight md:text-5xl xl:text-6xl">
-                The best offer <br /><span className="text-primary">for your business</span>
+                {Data.Heading} <br /><span className="text-primary"></span>
               </h1>
               <button className="bg-[grey] hover:bg[blue] mb-2 inline-block rounded bg-primary px-12 pt-4 pb-3.5 text-sm font-medium uppercase leading-normal  shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] md:mr-2 md:mb-0"
-                data-te-ripple-init data-te-ripple-color="light" href="#!" role="button">Get started</button>
+                data-te-ripple-init data-te-ripple-color="light" href="#!" role="button">{Data.Button}</button>
               <a className="inline-block rounded px-12 pt-4 pb-3.5 text-sm font-medium uppercase leading-normal text-primary transition duration-150 ease-in-out hover:bg-neutral-500 hover:bg-opacity-10 hover:text-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:text-primary-700 dark:hover:bg-neutral-700 dark:hover:bg-opacity-40"
                 data-te-ripple-init data-te-ripple-color="light" href="#!" role="button">Learn more</a>
             </div>
           </div>
           <div className="md:mb-12 lg:mb-0">
-            <img src="https://tecdn.b-cdn.net/img/new/ecommerce/vertical/004.jpg"
+            <img src={Data.Image}
               className="w-full rounded-lg shadow-lg dark:shadow-black/20" alt="" />
           </div>
         </div>

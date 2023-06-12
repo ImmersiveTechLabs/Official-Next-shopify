191efad0fb97c656eed877eb0913aa23e5795e1e
muhammadsaad91 <saadattari91@gmail.com>
2023-06-12 21:43:46 +0500

    collection images added

--- pages/collections/[collection].js
+++ pages/collections/[collection].js
@@ -101,7 +101,7 @@ return (
    
     <div className="bg-white">
     <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <h2 className="text-2xl font-extrabold text-gray-900 mb-6">
+      <h2 className="text-2xl font-extrabold text-gray-900 mb-6 uppercase	">
         {collection}
       </h2>
         <p className=" font-extrabold mb-6">
